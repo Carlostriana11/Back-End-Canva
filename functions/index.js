d761263c0193f36b3abeb9c7e21dc67a8506356f
Carlos Triana <carlostriana404@gmail.com>
2024-07-15 20:57:16 -0500

    A new CRUD was created to change the status of Canvas posts so that when the page is refreshed, posts remain where they were left.

--- functions/index.js
+++ functions/index.js
@@ -12,10 +12,18 @@ admin.initializeApp({
 
 const db = admin.firestore();
 
+app.post("/api/canva", async (req, res) =>{
+  await db.collection("tasksCanvas")
+      .doc()
+      .create({
+        description: req.body.description,
+        status: req.body.status || "todo"});
+  return res.status(200).json();
+} );
+
 app.get("/hello-word", (req, res)=>{
   return res.status(200).json({msg: "Hello Word"});
 });
-
 
 app.get("/api/canva", async (req, res) =>{
   try {
@@ -25,6 +33,8 @@ app.get("/api/canva", async (req, res) =>{
 
     const response = docs.map( (doc) => ({
       description: doc.data().description,
+      id: doc.id,
+      status: doc.data().status,
     }));
 
     return res.status(200).json(response);
@@ -63,10 +73,30 @@ app.put("/api/canva/:tasksCanvas_id", async (req, res) =>{
     await document.update({
       description: req.body.description,
     });
-    return res.status(200).json();
+    return res.status(200).json( {message: "Task updated successfully"});
   } catch (error) {
-    return res.status(500).json();
+    console.error("Error updating task status:", error);
+    return res.status(500).json({error: error.message});
+  }
+});
+app.put("/api/canva/:tasksCanvas_id/status", async (req, res) => {
+  try {
+    const {status} = req.body;
+
+    if (!status) {
+      return res.status(400).json({error: "Status is required."});
+    }
+
+    // eslint-disable-next-line max-len
+    const document = db.collection("tasksCanvas").doc(req.params.tasksCanvas_id);
+    await document.update({status});
+    return res.status(200).json({message: "Task status updated successfully"});
+  } catch (error) {
+    console.error("Error updating task status:", error);
+    return res.status(500).json({error: error.message});
   }
 });
 
 exports.app = functions.https.onRequest(app);
+
+
